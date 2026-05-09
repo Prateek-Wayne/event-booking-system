b7e68170e6e854606d19f78bd6d6942213edcc9e
Prateek-Wayne <lordprateekverma@gmail.com>
2026-05-09 23:16:26 +0530

    feat: enhance booking service with Kafka integration and update configurations

--- booking/src/main/java/com/ticketflow/booking/service/BookingService.java
+++ booking/src/main/java/com/ticketflow/booking/service/BookingService.java
@@ -2,6 +2,7 @@ package com.ticketflow.booking.service;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.ticketflow.booking.client.InventoryServiceClient;
@@ -11,13 +12,18 @@ import com.ticketflow.booking.dto.response.InventoryResponse;
 import com.ticketflow.booking.entity.Customer;
 import com.ticketflow.booking.repository.CustomerRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class BookingService {
 
     @Autowired
     private CustomerRepository customerRepository;
     @Autowired
     private InventoryServiceClient inventoryServiceClient;
+    @Autowired
+    private KafkaTemplate<String, BookingResponse> kadKafkaTemplate;
 
     public BookingResponse createBooking(BookingRequest bookingRequest) {
 
@@ -28,11 +34,13 @@ public class BookingService {
         }
 
         InventoryResponse event = inventoryServiceClient.getInventory(bookingRequest.getEventId());
-        if (event.getCapacity() < bookingRequest.getTicketCount()) {
+        if (event.getLeftCapacity() < bookingRequest.getTicketCount()) {
             throw new RuntimeException("no space left in event");
         }
 
         BookingResponse booking = createBooking(bookingRequest, customer.get(), event);
+        kadKafkaTemplate.send("booking", booking);
+        log.info("Booking send to kafka", booking);
 
         return booking;
     }
@@ -40,7 +48,8 @@ public class BookingService {
     static BookingResponse createBooking(BookingRequest bookingRequest, final Customer customer,
             InventoryResponse inventoryResponse) {
         return BookingResponse.builder().customerId(customer.getId()).ticketCount(bookingRequest.getTicketCount())
-                .totalPrice(inventoryResponse.getTicketPrice())
+                .totalPrice(inventoryResponse.getTicketPrice()
+                        .multiply(new java.math.BigDecimal(bookingRequest.getTicketCount())))
                 .eventId(inventoryResponse.getEventId())
                 .build();
 
