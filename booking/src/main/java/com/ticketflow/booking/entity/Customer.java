3d438828bc8dd4958d7ec98b25b2f07e8cc1ae1d
Prateek-Wayne <lordprateekverma@gmail.com>
2026-05-09 18:04:57 +0530

    init booking service

---
+++ booking/src/main/java/com/ticketflow/booking/entity/Customer.java
@@ -0,0 +1,30 @@
+package com.ticketflow.booking.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+}
