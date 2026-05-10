9af5c7a2f81998fbe26114c5a3b2b46171b08282
Prateek-Wayne <lordprateekverma@gmail.com>
2026-05-10 12:31:11 +0530

    feat: update BookingServiceRoutes to use dynamic booking service URL

--- apigateway/src/main/java/com/ticketflow/apigateway/routes/BookingServiceRoutes.java
+++ apigateway/src/main/java/com/ticketflow/apigateway/routes/BookingServiceRoutes.java
@@ -1,5 +1,6 @@
 package com.ticketflow.apigateway.routes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
@@ -10,18 +11,23 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
-import java.net.URI;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.setPath;
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
 
-import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+import java.net.URI;
 
 @Configuration
 public class BookingServiceRoutes {
 
+        @Value("${booking.service.url}")
+        String bookingServiceUrl;
+
         @Bean
         public RouterFunction<ServerResponse> bookingRoutes() {
                 return GatewayRouterFunctions.route("booking-service")
-                                .route(RequestPredicates.POST("/api/v1/booking"),
-                                                HandlerFunctions.http("http://localhost:8081/booking/addBooking"))
+                                .route(RequestPredicates.POST("/api/v1/booking"), HandlerFunctions.http())
+                                .before(uri(URI.create(bookingServiceUrl)))
+                                .before(setPath("/booking/addBooking"))
                                 .build();
         }
 }
